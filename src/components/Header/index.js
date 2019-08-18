72d3d5f659fc874aa087ef37f73dc74c0275a6cb
Your Name <xiejing418@gmail.com>
2019-08-17 23:39:26 -0700

    use react-bootstrap

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+function Header() {
+    return(
+        <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+        </p>
+        <p>
+        </p>
+        </Jumbotron>
+        );
+}
+
+export default Header;
---
+++ src/pages/Books.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+
+class Books extends Component {
+
+    render(){
+        return(
+            <Header/>
+        )
+    }
+
+}
+
+export default Books;

1a704c8155d818d219f1f15598b2fe33a4e40e22
Your Name <xiejing418@gmail.com>
2019-08-18 00:15:39 -0700

    jumbotron css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,13 +5,10 @@ import Image from "./books.jpg"
 function Header() {
     return(
         <Jumbotron style = {{backgroundImage: `url(${Image})`, backgroundSize: "contain"}}>
-        <h1>Hello, world!</h1>
-        <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-        </p>
-        <p>
-        </p>
+        <h1 style={{ color: "white", textShadow: "1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue", textAlign: "Center"}}>React Google Book Search</h1>
+        <h4 style={{textAlign: "Center", color: "white"}}>
+        Search for and save books of interest
+        </h4>
         </Jumbotron>
         );
 }

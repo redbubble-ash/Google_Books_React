66fc0cf389a5e2bbe8ebdc43e7d48bf5221d17cd
Your Name <xiejing418@gmail.com>
2019-08-18 20:31:43 -0700

    list saved books

--- src/pages/Saved.js
+++ src/pages/Saved.js
@@ -2,6 +2,10 @@ import React, {Component} from "react";
 import Header from "../components/Header";
 import SearchForm from "../components/SearchForm";
 import SearchButton from "../components/SearchButton";
+import ListResult from "../components/ListResult";
+import API from "../utils/API";
+import ListGroup from "react-bootstrap/ListGroup";
+
 
 
 
@@ -45,37 +49,66 @@ class Saved extends Component {
         return(
             (this.state.ifResults) ? (
                 <div className="container">
-                <div >
-                    <Header/>
-                    <SearchForm
-                    name="title"
-                    value={this.state.title}
-                    onChange = {this.handleInputChange}
-                    />
-                    <SearchButton
-                    onClick={this.handleFormSubmit}
-                    className="btn btn-info"
-                    />
-                </div>
+                    <div >
+                        <Header/>
+                        <SearchForm
+                        name="title"
+                        value={this.state.title}
+                        onChange = {this.handleInputChange}
+                        />
+                        <SearchButton
+                        onClick={this.handleFormSubmit}
+                        className="btn btn-info"
+                        />
+                    </div>
+                    <div>
+
+                    <h1 className="heading-title mx-sm-3 mb-2 text-center">Saved Books</h1>
+                    <ListResult>
+                    {this.state.books.map((book,index)=>(
+                        <ListGroup.Item key={book.id}>
+                            <div className="order-div">
+                                <a 
+                                href={book.volumeInfo.infoLink}
+                                target={this.state.target}
+                                >
+                                {book.volumeInfo.title}
+                                </a>
+                                <p>{book.volumeInfo.authors} (Author)</p>
+                                <p>
+                                <img align="left" style={{paddingRight:10}}
+                                    src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.authors}
+                                />
+                                    {book.volumeInfo.description}
+                                </p>
+                            </div>
+                        </ListGroup.Item>
+
+
+                    ))}
+                </ListResult>
+
+
+                    </div>
                 </div>
 
             ):(
                 <div className="container">
-                <div >
-                    <Header/>
-                    <SearchForm
-                    name="title"
-                    value={this.state.title}
-                    onChange = {this.handleInputChange}
-                    />
-                    <SearchButton
-                    onClick={this.handleFormSubmit}
-                    className="btn btn-info"
-                    />
-                </div>
-                <h3>
-                You have no saved books. Click search bar to find some.
-                </h3>
+                    <div >
+                        <Header/>
+                        <SearchForm
+                        name="title"
+                        value={this.state.title}
+                        onChange = {this.handleInputChange}
+                        />
+                        <SearchButton
+                        onClick={this.handleFormSubmit}
+                        className="btn btn-info"
+                        />
+                    </div>
+                    <h3>
+                    You have no saved books. Click search bar to find some.
+                    </h3>
 
                 </div>
 

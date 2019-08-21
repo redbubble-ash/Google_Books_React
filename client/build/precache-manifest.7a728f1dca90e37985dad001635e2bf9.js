b2ca0175ca7fdda9828fb3cc4babf24968adb32e
Jing Xie <xiejing418@gmail.com>
2019-08-20 21:24:46 -0700

    push to heroku

---
+++ client/build/precache-manifest.7a728f1dca90e37985dad001635e2bf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a500a2ab61ff135db592",
+    "url": "/static/css/main.9c8ed2e6.chunk.css"
+  },
+  {
+    "revision": "a500a2ab61ff135db592",
+    "url": "/static/js/main.2cb346f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d23e9418b7038bb70081",
+    "url": "/static/js/2.e6a9cef1.chunk.js"
+  },
+  {
+    "revision": "3ccb3ce4ac455beac2e27a9c10163e3a",
+    "url": "/static/media/books.3ccb3ce4.jpg"
+  },
+  {
+    "revision": "2619858d149b410532cf0294f07586d0",
+    "url": "/index.html"
+  }
+];

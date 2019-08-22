a55dd2469da5d418b1be5d44c76efd3b0a1575a0
Jason Rosen <jrosen@jasonrosen.com>
2019-08-21 22:26:17 -0700

    updated

---
+++ client/build/precache-manifest.56fc2ae20c5c978360130b5a6fc2fa8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ccb3ce4ac455beac2e27a9c10163e3a",
+    "url": "/static/media/books.3ccb3ce4.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5946ea91f7422bec60dd",
+    "url": "/static/js/main.c2cc35f8.chunk.js"
+  },
+  {
+    "revision": "3bb640ebde736204b4f4",
+    "url": "/static/js/2.90076c95.chunk.js"
+  },
+  {
+    "revision": "5946ea91f7422bec60dd",
+    "url": "/static/css/main.1b647888.chunk.css"
+  },
+  {
+    "revision": "3ac6d7072fd8f2d643d800815b607ba4",
+    "url": "/index.html"
+  }
+];
